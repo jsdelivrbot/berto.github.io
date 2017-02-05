96ae42dcec5a364c2481d24fa0ecacbf3e21f976
Roberto Ortega <berto.ort@gmail.com>
2017-02-05 01:46:27 -0700

    🚀 Deploying latest changes

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const burst = new mojs.Burst({
+  left: 0, 
+  top: 0,
+  radius:   { 0: 100 },
+  count:    7,
+  angle:    { 0: 90 },
+  opacity:  { 1: 0 },
+  children: {
+    fill:       { '#BE6CB6' : 'cyan' },
+    duration:   2000
+  }
+})
+
+document.addEventListener( 'click' , (e) => {
+  let isA = e.target.localName === 'a'
+  let hasParent = e.target.parentNode.localName === 'a'
+  let hasGrandparent = e.target.parentNode.parentNode.localName === 'a'
+  if (!isA && !hasParent && !hasGrandparent) {
+    burst
+      .tune({ x: e.pageX, y: e.pageY })
+      .generate()
+      .replay();
+  }
+})
