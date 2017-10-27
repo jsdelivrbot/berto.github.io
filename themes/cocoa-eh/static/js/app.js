b3f185271e3e74f8e0e72598ca20b26c1144953a
Roberto Ortega <berto.ort@gmail.com>
2017-10-27 15:51:56 -0600

    🌚 create dark theme

--- themes/cocoa-eh/static/js/app.js
+++ themes/cocoa-eh/static/js/app.js
@@ -11,30 +11,121 @@ const burst = new mojs.Burst({
   }
 })
 
+const displayBackground = location.pathname === '/' || location.pathname === '/about/'
 
-
-if (location.pathname === '/' || location.pathname === '/about/') {
+if (displayBackground) {
   const overlay = document.querySelector('.overlay')
-  overlay.style.opacity = .05
+  overlay.style.opacity = .1
 }
 
-document.addEventListener( 'click' , (e) => {
-  let isA = e.target.localName === 'a'
-  let hasParent = e.target.parentNode.localName === 'a'
-  let hasGrandparent = e.target.parentNode.parentNode.localName === 'a'
-  if (!isA && !hasParent && !hasGrandparent) {
-    burst
-      .tune({ x: e.pageX, y: e.pageY })
-      .generate()
-      .replay();
-  }
-})
+const img = document.querySelector('img')
+
+if (img) {
+  img.addEventListener( 'click' , (e) => {
+    let isA = e.target.localName === 'a'
+    let hasParent = e.target.parentNode.localName === 'a'
+    let hasGrandparent = e.target.parentNode.parentNode.localName === 'a'
+    if (!isA && !hasParent && !hasGrandparent) {
+      burst
+        .tune({ x: e.pageX, y: e.pageY })
+        .generate()
+        .replay();
+    }
+  })
+}
 
 console.set({size: '2em'});
 console.party('Welcome to my console!!');
 console.set({size: '1em', style: 'pink'});
 console.print('Nothing but beautiful text');
 console.log('https://github.com/berto/fun-logger');
+
+// Dark Theme
+
+const button = document.querySelector('.theme')
+const backgroundImage = document.querySelector('.overlay')
+const body = document.querySelector('body')
+initTheme()
+
+button.addEventListener('click', toggleTheme)
+
+function initTheme() {
+  const isLight = isLightTheme()
+  if (!isLight) {
+    button.classList.toggle('ion-ios-moon')
+    button.classList.toggle('ion-ios-sunny')
+  }
+  toggleCSS()
+}
+
+function toggleTheme(event) {
+  body.style.transition = 'background 1s'
+  event.target.classList.toggle('ion-ios-moon')
+  event.target.classList.toggle('ion-ios-sunny')
+  savePreferences()
+  toggleCSS()
+}
+
+function toggleCSS() {
+  const isLight = isLightTheme()
+  if (isLight) {
+    let lightCSS = {
+      background: 'white',
+      color: 'black',
+      opacity: 0.05,
+      filter: 'none'
+    }
+    setTheme(lightCSS)
+  } else {
+    let darkCSS = {
+      background: 'black',
+      color: 'lightgray',
+      opacity: 0.1,
+      filter: 'invert(100%)'
+    }
+    setTheme(darkCSS)
+  }
+}
+
+function savePreferences() {
+  const isLight = localStorage.getItem('isDark') != 'true'
+  let isDark = false
+  if (isLight) {
+    isDark = true
+  }
+  localStorage.setItem('isDark', isDark)
+}
+
+function isLightTheme(color) {
+  return localStorage.getItem('isDark') === null || localStorage.getItem('isDark') != 'true'
+}
+
+function setTheme(css) {
+  const title = document.querySelector('.name h1')
+  setElementsColor('section.main .content', css.color)
+  setElementsColor('section.main .content .markdown p', css.color)
+  for(let i = 1; i <= 6; i++) {
+    setElementsColor(`section.main .content .markdown h${i}`, css.color)
+  }
+  setElementsColor('section.main .content .page-heading', css.color)
+  setElementsColor('section.main .content .markdown code', 'black')
+  setElementsColor('section.main .container .content .see-more', css.color)
+  if (!displayBackground) {
+    css.opacity = 0
+  }
+  body.style.backgroundColor = css.background
+  title.style.color = css.color 
+  backgroundImage.style.opacity = css.opacity
+  backgroundImage.style.filter = css.filter
+  button.style.color = css.color
+}
+
+function setElementsColor(selector, color) {
+  const elements = document.querySelectorAll(selector)
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = color
+  }
+}
 
 // ZERGLING
 
